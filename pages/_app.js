9fd24da8fdff2c163eb7cee23a9a3cbe9f5466da
Prakash Satbhai <prakashs@winjit.com>
2022-04-19 18:11:17 +0530

    changes committed after global loading spinner

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,39 @@ import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
+import Router from "next/router";
+import { useState, useEffect } from "react";
+import LoadingSpinner from "../components/ui/LoadingSpinner";
 
 function MyApp({ Component, pageProps }) {
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("end");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <Component {...pageProps} />
+      )}
     </Layout>
   );
 }
